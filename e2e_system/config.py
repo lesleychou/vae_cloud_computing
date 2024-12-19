27a9cb7e0bbebce71a13e3b32c378eec02e872ac
lesley <lesleychou339@gmail.com>
2024-12-19 12:17:10 -0500

    add configs

--- e2e_system/config.py
+++ e2e_system/config.py
@@ -10,10 +10,10 @@ class Config():
         self.project_dir = os.path.dirname(os.path.abspath(__file__))
         self.pre_proc_method = "none"
         # General training hyperparams
-        self.batch_size = 100
+        self.batch_size = 10
         self.latent_dim = 12
         self.hidden_dim = 1024
-        self.n_epochs = 20
+        self.n_epochs = 100
         self.logging_freq = 1  # Number of epochs we should log the results to the user
         self.patience = 50  # How many epochs should we allow the model train to see if
         # improvement is made
